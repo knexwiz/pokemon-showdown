b2e0ee1ae1d707a5b3012f526be0c484705adc83
Spriing <knex12wiz@gmail.com>
2025-12-05 11:31:09 -0700

    Create custom-formats.ts

---
+++ data/custom-formats.ts
@@ -0,0 +1,14 @@
+export const Formats: FormatList = [
+
+	{
+        	section: "FDL Custom",
+    	},
+    	{
+        	name: "[Gen 9] Modern: Gen V",
+        	mod: 'gen9',
+        	gameType: 'doubles',
+        	bestOfDefault: true,
+        	ruleset: ['Flat Rules', '!! Adjust Level = 50', 'VGC Timer', 'Open Team Sheets', 'Terastal Clause', 'Standard NatDex'],
+    	},
+
+];
