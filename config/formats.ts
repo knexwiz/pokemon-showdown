9c801298ef0067c9111c397ca9a6442205140d8c
Spriing <knex12wiz@gmail.com>
2025-12-05 11:08:39 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -26,7 +26,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
     	},
     	{
         	name: "[Gen 9] Modern: Gen V",
-        	mod: 'gen5modern',
+        	mod: 'gen9',
         	gameType: 'doubles',
         	bestOfDefault: true,
         	ruleset: ['Flat Rules', '!! Adjust Level = 50', 'VGC Timer', 'Open Team Sheets', 'Terastal Clause', 'Standard NatDex'],
